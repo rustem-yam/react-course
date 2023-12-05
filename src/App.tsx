7e0f4ea73753a160e9e138b05827231d5d7a4565
rustem-yam <yamaltdinov.rustem2004@yandex.ru>
2023-12-05 19:05:26 +0300

    constants and columns config moved out of app function user interface moved to separate app interface file setPage now uses prev

--- src/App.tsx
+++ src/App.tsx
@@ -3,56 +3,57 @@ import { Button, Table } from "antd";
 import { ColumnsType } from "antd/es/table";
 import axios from "axios";
 import { useEffect, useState } from "react";
+import { IUser } from "./app.interface";
+
+const URL: string = "https://jsonplaceholder.typicode.com/users";
+const LIMIT_TOPIC: number = 2;
+
+const columns: ColumnsType<IUser> = [
+  {
+    key: "id",
+    title: "id",
+    dataIndex: "id",
+  },
+  {
+    key: "name",
+    title: "name",
+    dataIndex: "name",
+  },
+  {
+    key: "email",
+    title: "email",
+    dataIndex: "email",
+  },
+];
 
 function App() {
   const [page, setPage] = useState<number>(1);
 
   useEffect(() => {
-    getUsers(page, 2);
+    getUsers(page, LIMIT_TOPIC);
   }, [page]);
-
-  interface IUser {
-    id: number;
-    name: string;
-    email: string;
-  }
 
   const [dataSource, setDataSource] = useState<IUser[]>();
 
   const getUsers = async (page: number, limit: number) => {
     const response = await axios.get<IUser[]>(
-      `https://jsonplaceholder.typicode.com/users?_limit=${limit}&_page=${page}`
+      `${URL}?_limit=${limit}&_page=${page}`
     );
     setDataSource(response.data);
   };
-
-  const columns: ColumnsType<IUser> = [
-    {
-      key: "id",
-      title: "id",
-      dataIndex: "id",
-    },
-    {
-      key: "name",
-      title: "name",
-      dataIndex: "name",
-    },
-    {
-      key: "email",
-      title: "email",
-      dataIndex: "email",
-    },
-  ];
 
   return (
     <>
       <Table columns={columns} dataSource={dataSource} />
       <div className="table__buttons">
-        <Button onClick={() => setPage(page - 1)} disabled={!(page - 1)}>
+        <Button
+          onClick={() => setPage((prev) => prev - 1)}
+          disabled={!(page - 1)}
+        >
           Previous
         </Button>
         <p>{page}</p>
-        <Button onClick={() => setPage(page + 1)}>Next</Button>
+        <Button onClick={() => setPage((prev) => prev + 1)}>Next</Button>
       </div>
     </>
   );
