30dd8fbabf97d152b9c172924f53258025d6bf4a
rustem-yam <yamaltdinov.rustem2004@yandex.ru>
2023-12-05 06:33:24 +0300

    table pagination buttons added

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,30 @@
 import "./App.css";
-import { Table } from "antd";
+import { Button, Table } from "antd";
 import { ColumnsType } from "antd/es/table";
+import axios from "axios";
+import { useEffect, useState } from "react";
 
 function App() {
+  const [page, setPage] = useState<number>(1);
+
+  useEffect(() => {
+    getUsers(page, 2);
+  }, [page]);
+
   interface IUser {
-    key: number;
     id: number;
     name: string;
     email: string;
   }
+
+  const [dataSource, setDataSource] = useState<IUser[]>();
+
+  const getUsers = async (page: number, limit: number) => {
+    const response = await axios.get<IUser[]>(
+      `https://jsonplaceholder.typicode.com/users?_limit=${limit}&_page=${page}`
+    );
+    setDataSource(response.data);
+  };
 
   const columns: ColumnsType<IUser> = [
     {
@@ -28,30 +44,16 @@ function App() {
     },
   ];
 
-  const data: IUser[] = [
-    {
-      key: 0,
-      id: 0,
-      name: "Jack",
-      email: "example@ex.com",
-    },
-    {
-      key: 1,
-      id: 1,
-      name: "Ann",
-      email: "test@ex.com",
-    },
-    {
-      key: 2,
-      id: 2,
-      name: "George",
-      email: "hello@ex.com",
-    },
-  ];
-
   return (
     <>
-      <Table columns={columns} dataSource={data} />
+      <Table columns={columns} dataSource={dataSource} />
+      <div className="table__buttons">
+        <Button onClick={() => setPage(page - 1)} disabled={!(page - 1)}>
+          Previous
+        </Button>
+        <p>{page}</p>
+        <Button onClick={() => setPage(page + 1)}>Next</Button>
+      </div>
     </>
   );
 }
